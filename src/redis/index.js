36290d20ebfd18c605b09aab491d897297cda208
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-06-09 20:08:09 +0900

    APIから取得したデータをRedisに格納

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,7 +1,10 @@
 const redis = require("redis");
 const express = require("express");
-const expressPort = 3000;
+const axios = require("axios");
+
+// redis, express ports
 const redisPort = 6379;
+const expressPort = 3000;
 
 // redis config
 const redisConfig = {
@@ -10,25 +13,56 @@ const redisConfig = {
 };
 
 // redis
-const client = redis.createClient(redisConfig);
+const redisClient = redis.createClient(redisConfig);
 // express
 const app = express();
+// mock api
+const MOCK_API = "https://jsonplaceholder.typicode.com/users/";
 
-client.on("connect", function () {
-	console.log("Redisへ接続");
+app.get("/users", (req, res) => {
+	const id = req.query.id;
+
+	try {
+		axios.get(`${MOCK_API}?id=${id}`).then((response) => {
+			const users = response.data;
+			console.log(users);
+			res.status(200).send(users);
+		});
+	} catch (err) {
+		res.status(500).send({ error: err.message });
+	}
 });
 
-app.get("/", (req, res) => {
-	client.set("sato", "value");
-	res.send("Hello World");
-});
+app.get("/cached-users", (req, res) => {
+	const id = req.query.id;
 
-app.get("/sato", (req, res) => {
-	const sato = client.get("sato", function (err, value) {
-		res.send(value);
-	});
+	try {
+		redisClient.get(id, (err, data) => {
+			if (err) {
+				console.log(err);
+				throw err;
+			}
+
+			if (data) {
+				console.log("User successfully retrieved from Redis");
+				res.status(200).send(JSON.parse(data));
+			} else {
+				axios.get(`${MOCK_API}?id=${id}`).then((response) => {
+					const users = response.data;
+					console.log(users);
+					redisClient.setex(id, 600, JSON.stringify(users));
+
+					console.log("Users successfully retrieved from the API");
+
+					res.status(200).send(users);
+				});
+			}
+		});
+	} catch (err) {
+		res.status(500).send({ error: err.message });
+	}
 });
 
 app.listen(expressPort, () => {
-	console.log(`Start on port ${expressPort}`);
+	console.log(`Server Start on Port ${expressPort}`);
 });
